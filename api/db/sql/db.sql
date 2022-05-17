62de9fe67540b2cf4c7c2cc9638cbdcd36888dcb
Carlos Briones <carlosbr-c@herbalife.com>
2022-05-17 11:45:32 -0500

    Ordering tables

--- api/db/sql/db.sql
+++ api/db/sql/db.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE cities_db;
 
-CREATE TABLE timezones(
-    timezone_id serial NOT NULL,
-    timezone varchar(10) NOT NULL,
-    city_id integer NOT NULL,
-    PRIMARY KEY(timezone_id),
-    CONSTRAINT fk_cities__city_id FOREIGN KEY (city_id) REFERENCES cities(city_id)  
-);
-
 CREATE TABLE cities(
     city_id serial NOT NULL,
     city_name varchar(50) NOT NULL,
     continent varchar(50) NOT NULL,
     PRIMARY KEY(city_id)    
+);
+
+CREATE TABLE timezones(
+    timezone_id serial NOT NULL,
+    timezone varchar(10) NOT NULL,
+    city_id integer NOT NULL,
+    PRIMARY KEY(timezone_id),
+    CONSTRAINT fk_cities__city_id FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
